fc0169e1ad10e62882f294216696b025fd4c43c9
mateo <mateotasso@gmail.com>
2023-07-04 23:43:23 -0300

    change title

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import confetti from 'canvas-confetti'
 
 import { Square } from './components/Square.jsx'
@@ -55,6 +55,10 @@ function App () {
       setWinner(false) // empate
     }
   }
+
+  useEffect(() => {
+    console.log('useEffect')
+  })
 
   return (
     <main className='board'>
